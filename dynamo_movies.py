92229f187a1977223638a79220880bdbb864b384
Froslee17 <alex.froslee@drake.edu>
2026-02-26 11:35:51 -0600

    Create dynamo_movies.py

---
+++ dynamo_movies.py
@@ -0,0 +1,17 @@
+# CS178 - Lab 8 Starter
+
+REGION = "us-east-1"
+TABLE_NAME = "Movies"
+
+def print_movie(movie):
+    print(f"Title: {movie['Title']}, Year: {movie['Year']}")
+
+def print_all_movies():
+    # TODO: connect to DynamoDB and print all movies
+    pass
+
+def main():
+    print_all_movies()
+
+if __name__ == "__main__":
+    main()
